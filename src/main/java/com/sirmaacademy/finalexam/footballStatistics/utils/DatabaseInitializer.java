2ef282f9b1e471d300e70d63eeaf2395a1b819d3
Georgi <hristovgeorgivalentinov@gmail.com>
2024-09-07 21:13:51 +0300

    implement CommandLineRunner

--- src/main/java/com/sirmaacademy/finalexam/footballStatistics/utils/DatabaseInitializer.java
+++ src/main/java/com/sirmaacademy/finalexam/footballStatistics/utils/DatabaseInitializer.java
@@ -1,2 +1,26 @@
-package com.sirmaacademy.finalexam.footballStatistics.utils;public class DatabaseInitializer {
+package com.sirmaacademy.finalexam.footballStatistics.utils;
+
+import com.sirmaacademy.finalexam.footballStatistics.repository.CSVToDatabaseInsert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DatabaseInitializer implements CommandLineRunner {
+
+    private final CSVToDatabaseInsert csvToDatabaseInsert;
+
+    @Autowired
+    public DatabaseInitializer(CSVToDatabaseInsert csvToDatabaseInsert) {
+        this.csvToDatabaseInsert = csvToDatabaseInsert;
+    }
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+       // this.csvToDatabaseInsert.insertTeamsCSVFile(ReadCSV.getTeamsList());
+
+    }
+
 }
