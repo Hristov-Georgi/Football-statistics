edfb6ea93d6548b93af7fce1a581b8e88fd41fab
Georgi <hristovgeorgivalentinov@gmail.com>
2024-09-09 17:57:51 +0300

    implement CommandLineRuner to populate DB with data from the csv files when program starts

--- src/main/java/com/sirmaacademy/finalexam/footballStatistics/utils/DatabaseInitializer.java
+++ src/main/java/com/sirmaacademy/finalexam/footballStatistics/utils/DatabaseInitializer.java
@@ -1,6 +1,6 @@
 package com.sirmaacademy.finalexam.footballStatistics.utils;
 
-import com.sirmaacademy.finalexam.footballStatistics.repository.CSVToDatabaseInsert;
+import com.sirmaacademy.finalexam.footballStatistics.service.CsvToDatabaseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -8,18 +8,20 @@ import org.springframework.stereotype.Component;
 @Component
 public class DatabaseInitializer implements CommandLineRunner {
 
-    private final CSVToDatabaseInsert csvToDatabaseInsert;
+    private final CsvToDatabaseService csvToDatabaseService;
 
     @Autowired
-    public DatabaseInitializer(CSVToDatabaseInsert csvToDatabaseInsert) {
-        this.csvToDatabaseInsert = csvToDatabaseInsert;
+    public DatabaseInitializer(CsvToDatabaseService csvToDatabaseService) {
+        this.csvToDatabaseService = csvToDatabaseService;
     }
-
 
     @Override
     public void run(String... args) throws Exception {
 
-       // this.csvToDatabaseInsert.insertTeamsCSVFile(ReadCSV.getTeamsList());
+        this.csvToDatabaseService.persistTeamsToDatabase();
+        this.csvToDatabaseService.persistPlayersToDatabase();
+        this.csvToDatabaseService.persistScoresAndMatchesToDatabase();
+        this.csvToDatabaseService.persistRecordsToDatabase();
 
     }
 
