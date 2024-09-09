4ca2dea743cad3e7917c3c9e2fbfcfd6a3af4565
Georgi <hristovgeorgivalentinov@gmail.com>
2024-09-09 15:49:30 +0300

    implement methods to validate and to proceed data from given csv files in database

--- src/main/java/com/sirmaacademy/finalexam/footballStatistics/repository/CsvToDatabaseRepository.java
+++ src/main/java/com/sirmaacademy/finalexam/footballStatistics/repository/CsvToDatabaseRepository.java
@@ -2,7 +2,6 @@ package com.sirmaacademy.finalexam.footballStatistics.repository;
 
 import com.sirmaacademy.finalexam.footballStatistics.model.entity.Match;
 import com.sirmaacademy.finalexam.footballStatistics.model.entity.Player;
-import com.sirmaacademy.finalexam.footballStatistics.model.entity.Score;
 import com.sirmaacademy.finalexam.footballStatistics.model.entity.Team;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
@@ -12,39 +11,26 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public class CSVToDatabaseInsert {
+public class CsvToDatabaseRepository {
 
     @PersistenceContext
     private EntityManager entityManager;
 
     @Transactional
-    public void insertMatchesFromCSVFile(List<Match> matches) {
+    public void saveMatches(List<Match> matches) {
 
         for (Match m : matches) {
-
             entityManager.createNativeQuery("INSERT INTO matches (id, local_date)" +
                     " VALUES (?, ?)")
                     .setParameter(1, m.getId())
                     .setParameter(2, m.getLocalDate())
                     .executeUpdate();
-
-            for (Score sc : m.getScores()) {
-
-// Exception may occur -> check DB relation table column names.
-
-                entityManager.createNativeQuery("INSERT INTO matches_scores (match_id, score_id)" +
-                                " VALUES (?, ?)")
-                        .setParameter(1, m.getId())
-                        .setParameter(2, sc.getId())
-                        .executeUpdate();
-            }
-
         }
 
     }
 
     @Transactional
-    public void insertPlayersFromCSVFile(List<Player> players) {
+    public void savePlayers(List<Player> players) {
 
         for (Player p : players) {
             entityManager.createNativeQuery("INSERT INTO players (id, field_position, full_name, team_number, team_id)" +
@@ -60,16 +46,21 @@ public class CSVToDatabaseInsert {
     }
 
     @Transactional
-    public void insertTeamsFromCSVFile(List<Team> teams) {
+    public void saveTeams(List<Team> teams) {
 
         for (Team t : teams) {
-            entityManager.createNativeQuery("INSERT INTO teams (id, name, manager_full_name, football_group)" +
-                    " VALUES (?, ?, ?, ?)")
-                    .setParameter(1, t.getId())
-                    .setParameter(2, t.getName())
-                    .setParameter(3, t.getManagerFullName())
-                    .setParameter(4, t.getFootballGroup().getValue())
-                    .executeUpdate();
+            try {
+                entityManager.createNativeQuery("INSERT INTO teams (id, name, manager_full_name, football_group)" +
+                                " VALUES (?, ?, ?, ?)")
+                        .setParameter(1, t.getId())
+                        .setParameter(2, t.getName())
+                        .setParameter(3, t.getManagerFullName())
+                        .setParameter(4, t.getFootballGroup().name())
+                        .executeUpdate();
+            } catch (Exception e) {
+                //TODO: put correct exception.
+            }
+
         }
 
     }
--- src/main/java/com/sirmaacademy/finalexam/footballStatistics/service/CsvToDatabaseService.java
+++ src/main/java/com/sirmaacademy/finalexam/footballStatistics/service/CsvToDatabaseService.java
@@ -1,2 +1,149 @@
-package com.sirmaacademy.finalexam.footballStatistics.service;public class CSVToDatabaseService {
+package com.sirmaacademy.finalexam.footballStatistics.service;
+
+import com.sirmaacademy.finalexam.footballStatistics.exceptions.*;
+import com.sirmaacademy.finalexam.footballStatistics.model.dto.MatchCsvDto;
+import com.sirmaacademy.finalexam.footballStatistics.model.dto.PlayerCsvDto;
+import com.sirmaacademy.finalexam.footballStatistics.model.dto.TeamCsvDto;
+import com.sirmaacademy.finalexam.footballStatistics.model.entity.Match;
+import com.sirmaacademy.finalexam.footballStatistics.model.entity.Player;
+import com.sirmaacademy.finalexam.footballStatistics.model.entity.Score;
+import com.sirmaacademy.finalexam.footballStatistics.model.entity.Team;
+import com.sirmaacademy.finalexam.footballStatistics.model.enums.FieldPosition;
+import com.sirmaacademy.finalexam.footballStatistics.model.enums.FootballGroup;
+import com.sirmaacademy.finalexam.footballStatistics.repository.CsvToDatabaseRepository;
+import com.sirmaacademy.finalexam.footballStatistics.utils.DateFormatter;
+import com.sirmaacademy.finalexam.footballStatistics.utils.ReadCSV;
+import com.sirmaacademy.finalexam.footballStatistics.validation.ValidateCsvDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CsvToDatabaseService {
+    private static final Logger logger = LoggerFactory.getLogger(CsvToDatabaseService.class);
+
+    private final ReadCSV readCSV;
+    private final CsvToDatabaseRepository csvToDatabaseRepository;
+    private final TeamService teamService;
+    private final ScoreService scoreService;
+
+    @Autowired
+    public CsvToDatabaseService(ReadCSV readCSV, CsvToDatabaseRepository csvToDatabaseRepository, TeamService teamService, ScoreService scoreService) {
+        this.readCSV = readCSV;
+        this.csvToDatabaseRepository = csvToDatabaseRepository;
+        this.teamService = teamService;
+        this.scoreService = scoreService;
+    }
+
+    /**
+     * I know it is not recommended. Method should do only one operation.
+     * But all data comes from matches.csv file and according to my project structure
+     * this is the optimal decision I make, because I read only once the file data.
+     */
+    public void persistScoresAndMatchesToDatabase() {
+        List<Match> matches = new ArrayList<>();
+        List<Score> scores = new ArrayList<>();
+
+        for (MatchCsvDto m : this.readCSV.extractMatchesData()) {
+
+            try {
+
+                Long id = ValidateCsvDto.validateId(m.getId());
+                Long aTeamId = ValidateCsvDto.validateId(m.getaTeamId());
+                Long bTeamId = ValidateCsvDto.validateId(m.getbTeamId());
+
+                if (!this.teamService.isTeamExist(aTeamId)) {
+                    throw new InvalidTeamException("Team with id: '" + aTeamId + "' does not exist.");
+                } else if (!this.teamService.isTeamExist(bTeamId)) {
+                    throw new InvalidTeamException("Team with id: '" + bTeamId + "' does not exist.");
+                }
+                LocalDate localDate = DateFormatter.formatDate(m.getDate());
+                String aTeamScore = ValidateCsvDto.validateGoalsInput(m.getScore(), "aTeamScore");
+                String bTeamScore = ValidateCsvDto.validateGoalsInput(m.getScore(), "bTeamScore");
+
+                Match match = new Match(id, localDate);
+                matches.add(match);
+
+                Score aTeam = new Score(teamService.findById(aTeamId), aTeamScore, match);
+                Score bTeam = new Score(teamService.findById(bTeamId), bTeamScore, match);
+                scores.add(aTeam);
+                scores.add(bTeam);
+
+            } catch(InvalidTeamException
+                    | InvalidIdException
+                    | DateTimeParseException
+                    | InvalidMatchScoreException e) {
+                logger.warn("Warning in matches.csv data: {}", e.getMessage());
+
+            } catch (IllegalStateException | IllegalArgumentException e) {
+                logger.warn("Something went wrong with regex pattern for match score validation. Warn: {}", e.getMessage());
+            }
+
+        }
+        this.csvToDatabaseRepository.saveMatches(matches);
+        this.scoreService.saveAllScores(scores);
+    }
+
+    public void persistPlayersToDatabase() {
+        List<Player> players = new ArrayList<>();
+
+        for (PlayerCsvDto p : this.readCSV.extractPlayersData()) {
+
+            try {
+                Long id = ValidateCsvDto.validateId(p.getId());
+                int teamNumber = ValidateCsvDto.validateTeamNumber(p.getTeamNumber());
+                FieldPosition fieldPosition = ValidateCsvDto.validateFieldPosition(p.getPosition());
+                String fullName = ValidateCsvDto.validatePersonFullName(p.getFullName());
+                Long teamId = ValidateCsvDto.validateId(p.getTeamId());
+
+                if (this.teamService.isTeamExist(teamId)) {
+                    Player player = new Player(id, teamNumber, fieldPosition, fullName, teamId);
+                    players.add(player);
+                } else {
+                    throw new InvalidTeamException("Team with id: '" + teamId + "' does not exist.");
+                }
+
+            } catch (InvalidIdException
+                     | InvalidSymbolException
+                     | InvalidLengthException
+                     | InvalidFootballGroupException
+                     | InvalidPlayerTeamNumberException
+                     | InvalidTeamException e) {
+                logger.warn("Players data Warn: {}", e.getMessage());
+            }
+
+        }
+        this.csvToDatabaseRepository.savePlayers(players);
+    }
+
+    public void persistTeamsToDatabase() {
+        List<Team> teams = new ArrayList<>();
+
+        for (TeamCsvDto t : this.readCSV.extractTeamsData()) {
+
+            try {
+                Long id = ValidateCsvDto.validateId(t.getId());
+                String name = ValidateCsvDto.validateTeamName(t.getName());
+                String managerFullName = ValidateCsvDto.validatePersonFullName(t.getManagerFullName());
+                FootballGroup footballGroup = ValidateCsvDto.validateFootballGroup(t.getFootballGroup());
+
+                Team team = new Team(id, name, managerFullName, footballGroup);
+                teams.add(team);
+            } catch (InvalidIdException
+                     | InvalidSymbolException
+                     | InvalidLengthException
+                     | InvalidFootballGroupException e) {
+                logger.warn("Teams data Warn: {}", e.getMessage());
+            }
+
+        }
+        this.csvToDatabaseRepository.saveTeams(teams);
+    }
+
 }
